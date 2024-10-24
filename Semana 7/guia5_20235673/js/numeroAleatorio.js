84f204b4d1778298e5012fd2006cfb2cd2fdce25
alexisc31 <inversorcentenial@gmail.com>
2024-10-23 23:56:07 -0600

    guia 5 e index
    actualizacion de archivos

--- Semana 7/guia5_20235673/js/numeroAleatorio.js
+++ Semana 7/guia5_20235673/js/numeroAleatorio.js
@@ -1,15 +1,53 @@
-//Numero aleatorio en el rango del 1 al 25
-const numeroAleatorio = Math.floor(Math.random() *25) + 1;
-//Constante para generar el numero de intentos=3
+// Número aleatorio en el rango del 1 al 25
+const numeroAleatorio = Math.floor(Math.random() * 25) + 1;
+// Constante para generar el número de intentos = 3
 const numeroIntentos = 3;
-//Guardar el numero de intentos realizados
+// Guardar el número de intentos realizados
 let intentos = 1;
-function generarNumeroAleatorio(){
-    //Definimos una variable para impresion de mensajes
-    let mensaje;
-    // Utilizamos el dom para acceder al parrafo creado
-    const parrafo = document.querySelector("#idParrado");
-    //Verificar en que intentos esta el usuario
-    if (intentos <= numeroIntentos){
-        let numero = prompt(
-            "Qué número se ha generado (Intento  " 
+
+function generarNumeroAleatorio() {
+  // Definimos una variable para impresión de mensajes
+  let mensaje;
+  // Utilizamos el DOM para acceder al párrafo creado
+  const parrafo = document.querySelector("#idParrafo");
+  // Verificar en qué intentos está el usuario
+  if (intentos <= numeroIntentos) {
+    let numero = prompt(
+      "¿Qué número se ha generado (Intento " + intentos + ")?"
+    );
+    // Verificar el número ingresado con el número correcto
+    if (numero == numeroAleatorio) {
+      mensaje =
+        "Es sorprendente, pudiste adivinar el número oculto (" +
+        numeroAleatorio +
+        "). Refresque la página para volver a jugar.";
+    } else if (intentos == numeroIntentos) {
+      mensaje =
+        "Su número de intentos ha terminado. El número oculto era: " +
+        numeroAleatorio +
+        ". Refresque la página para volver a jugar.";
+    } else {
+      // Comparar el número ingresado con el número aleatorio
+      if (numero < numeroAleatorio) {
+        mensaje =
+          "El número que ingresaste es más bajo. Vuelve a intentar. Quedan " +
+          (numeroIntentos - intentos) +
+          " intentos.";
+      } else {
+        mensaje =
+          "El número que ingresaste es más alto. Vuelve a intentar. Quedan " +
+          (numeroIntentos - intentos) +
+          " intentos.";
+      }
+    }
+    // Aumentar el valor de los intentos
+    intentos++;
+  } else {
+    mensaje =
+      "Su número de intentos ha terminado. El número oculto era: " +
+      numeroAleatorio +
+      ". Refresque la página para volver a jugar.";
+  }
+
+  parrafo.innerHTML = mensaje;
+}
